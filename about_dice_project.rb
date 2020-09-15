26aaf5c0161949b2db044b9a30ab1482272afe2f
Arthur Borges <arthurborges.dev@gmail.com>
2020-09-15 10:14:27 -0300

    Finished dice_project, inheritance and modules

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,6 +5,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_accessor :values
+  def initialize
+    @values = []
+  end
+
+  def roll(tries)
+    tries.times {
+      @values = (1..tries).map{ rand(6) + 1 }
+    }
+    @values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
